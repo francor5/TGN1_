5114bdfa62e26516a70e3659a5f775d464bec599
francor5 <89144104+francor5@users.noreply.github.com>
2021-10-21 09:09:59 -0300

    Add files via upload

---
+++ ParaAcomodadores.java
@@ -0,0 +1,9 @@
+package TRABAJOLAB;
+
+public interface ParaAcomodadores {
+    Salas getSalas();
+    void SetSala(Salas sala);
+
+
+
+}
---
+++ Persona.java
@@ -0,0 +1,36 @@
+package TRABAJOLAB;
+
+public abstract class Persona {
+    private String nombre;
+    private int edad;
+
+    public Persona(String nombre, int edad) {
+        this.nombre = nombre;
+        this.edad= edad;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    @Override
+    public abstract String toString();
+    public abstract String getTipo();
+
+
+
+
+}
+
